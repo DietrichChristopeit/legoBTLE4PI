c6c56bc7e7ae20e419036bdb614cf9dcbf164790
Dietrich Christopeit <dietch@esper.co.uk>
2020-12-26 02:21:10 +0100

    Clean up Testscripts.main

--- Tests/main.py
+++ Tests/main.py
@@ -1,16 +1,24 @@
 from time import sleep
 
 from Hub.HubTypes import HubNo2
-from Konstanten.Motor import Motor
+from Konstanten.Motor import Motor as Motorkonstante
 from Konstanten.Anschluss import Anschluss
 from Motor.EinzelMotor import EinzelMotor
 
+
 class Testscripts:
-    def __init__(self, MACaddress:str='90:84:2B:5E:CF:1F'):
+    def __init__(self, MACaddress: str = '90:84:2B:5E:CF:1F'):
+        """Testscript-Sammlung"""
+
         self.jeep = HubNo2(MACaddress)
 
     def alleMotoren(self):
+        """Testet alle Motoren.
+            * Vorderradantrieb / Hinterradantrieb einzeln /gemeinsam für Zeiten t
+            * Lenkung links / rechts 55° / 100°
 
+        :return:
+        """
         print('Controller Name:', self.jeep.leseControllerName.decode('UTF-8'))
 
         vorderradantrieb = EinzelMotor(Anschluss.A, "Vorderradantrieb")
@@ -22,29 +30,29 @@ class Testscripts:
         sleep(1.5)
         print("Drehe Vorderräder für 2560ms mit halber Kraft vorwärts...")
         sleep(0.5)
-        characteristicV = vorderradantrieb.dreheMotorFuerT(2560, Motor.VOR, 50, Motor.BREMSEN)
+        characteristicV = vorderradantrieb.dreheMotorFuerT(2560, Motorkonstante.VOR, 50, Motorkonstante.BREMSEN)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicV), withResponse=False)
         sleep(1.5)
         print("Drehe Hinterräder für 2560ms mit halber Kraft vorwärts...")
         sleep(0.5)
-        characteristicH = hinterradantrieb.dreheMotorFuerT(2560, Motor.VOR, 50, Motor.BREMSEN)
+        characteristicH = hinterradantrieb.dreheMotorFuerT(2560, Motorkonstante.VOR, 50, Motorkonstante.BREMSEN)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicH), withResponse=False)
         sleep(1.5)
-        print("Drehe Vorder- und Hinterräder gemeinsam für 4000ms mit halber Kraft rückwärts..")
+        print("Drehe Vorder- und Hinterräder gemeinsam für 4000ms mit voller Kraft rückwärts..")
         sleep(0.5)
-        characteristicV = vorderradantrieb.dreheMotorFuerT(4000, Motor.ZURUECK, 50, Motor.BREMSEN)
-        characteristicH = hinterradantrieb.dreheMotorFuerT(4000, Motor.ZURUECK, 50, Motor.BREMSEN)
+        characteristicV = vorderradantrieb.dreheMotorFuerT(4000, Motorkonstante.ZURUECK, 100, Motorkonstante.BREMSEN)
+        characteristicH = hinterradantrieb.dreheMotorFuerT(4000, Motorkonstante.ZURUECK, 100, Motorkonstante.BREMSEN)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicV), withResponse=False)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicH), withResponse=False)
         sleep(6)
         print("Lenke um 55° mit halber Kraft nach links...")
         sleep(0.5)
-        characteristicL = lenkung.dreheMotorFuerGrad(55, Motor.LINKS, 50, Motor.BREMSEN)
+        characteristicL = lenkung.dreheMotorFuerGrad(55, Motorkonstante.LINKS, 50, Motorkonstante.BREMSEN)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicL), withResponse=False)
         sleep(1.5)
         print("Lenke um 100° mit halber Kraft nach rechts...")
         sleep(0.5)
-        characteristicR = lenkung.dreheMotorFuerGrad(100, Motor.RECHTS, 50, Motor.BREMSEN)
+        characteristicR = lenkung.dreheMotorFuerGrad(100, Motorkonstante.RECHTS, 50, Motorkonstante.BREMSEN)
         self.jeep.holeController.writeCharacteristic(0x0e, bytes.fromhex(characteristicR), withResponse=False)
         sleep(1.5)
         print("Trenne Verbindung...")
@@ -52,6 +60,6 @@ class Testscripts:
         print("***Programmende***")
 
 
-
 if __name__=='__main__':
-    jeep =
+    test = Testscripts()
+    test.alleMotoren()
