76c8d12c76a62677d6807d7010b6766f0c2e901a
Dietrich Christopeit <dietch@esper.co.uk>
2021-01-05 03:38:42 +0100

    messaging working now

--- MessageHandling/PubDPSub.py
+++ MessageHandling/PubDPSub.py
@@ -11,7 +11,7 @@ from Geraet.Motor import Motor
 from MessageHandling.Pipeline import Pipeline
 
 
-#@deprecated(reason="Untidy design.", replacement="PublishingDelegate(MessageEntity)")
+# @deprecated(reason="Untidy design.", replacement="PublishingDelegate(MessageEntity)")
 class Notification(bluepy.btle.DefaultDelegate):
 
     def __init__(self):
@@ -86,7 +86,7 @@ class PublishingDelegate(MessagingEntity, bluepy.btle.DefaultDelegate):
         print("In Publisher")
 
     def handleNotification(self, cHandle, data):
-        self._pipeline.set_message(data, "Devices")
+        self._pipeline.set_message(data, "SND[DEVICES]")
 
     @property
     def uid(self) -> int:
@@ -128,21 +128,3 @@ class Dispatcher:
         for e in entities:
             self.pipelines[e.uid] = Queue(maxsize=20)  # MessageEntities registered at Dispatcher
             e.pipeline = self.pipelines[e.uid]
-
-#@deprecated(reason="Wrong Design, not necessary.", replacement="None.")
-class PublisherToDispatcherSubSystem:
-
-    def __init__(self):
-        self.dispatcher = Dispatcher("Dispatch notifications to KMotor attributes")
-        self.publisher = PublishingDelegate("LEGO TECHNIC HUB publisher")
-        self.dispatcher.registerPipelines({self.publisher})
-
-    def executeLoop(self):
-        pipeline_PubDisp = Pipeline()
-        event = threading.Event()
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(self.publisher, pipeline_PubDisp, event)
-            executor.submit(self.dispatcher, pipeline_PubDisp, event)
-            time.sleep(0.1)
-            logging.info("Main: about to set event")
-            event.set()
