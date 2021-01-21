789ad6207f53a538c3817071814fd4da29157c40
Dietrich Christopeit <dietch@esper.co.uk>
2021-01-21 17:36:52 +0100

    multiprocessing tests enhancements

--- Tests/TestMP_1.py
+++ Tests/TestMP_1.py
@@ -21,22 +21,54 @@
 #  SOFTWARE.
 
 from multiprocessing import Process, Value, Array, Queue, Event, Condition
+from queue import Empty
 from random import uniform, randint
 from time import sleep
 
 
-class Producer:
-
-    def __init__(self, execQ: Queue, terminate: Event):
+class Commander:
+    def __init__(self, execQ: Queue, terminate: Event, cvPortFree: Condition, portFree: Event):
         self._execQ = execQ
+        self._terminate: Event = terminate
+        self._cvPortFree = cvPortFree
+        self._cvPortFree1: Condition = Condition()
+        self._portFree = portFree
+
+    def prod(self):
+        print("[{}]-[MSG]: STARTED...".format(Process.name))
+        while not self._terminate.is_set():
+            e = randint(6, 11)
+            print("[{}]-[SND]: WAITING TO SEND {}".format(Process.name, e))
+            with self._cvPortFree:
+                self._cvPortFree.wait_for(lambda: self._portFree.is_set())
+                self._portFree.clear()
+                self._execQ.put(e)
+                print("[{}]-[SND]: COMMAND SENT {}".format(Process.name, e))
+                self._cvPortFree.notify_all()
+            sleep(0.0001)
+
+        print("[{}]-[MSG]: SHUTTING DOWN...".format(Process.name))
+        return
+
+
+class Delegate:
+
+    def __init__(self, cmdQ: Queue, rsltQ: Queue, terminate: Event):
+        self._cmdQ = cmdQ
+        self._rsltQ = rsltQ
         self._terminate: Event = terminate
 
     def prod(self):
         print("[{}]-[MSG]: STARTED...".format(Process.name))
         while not self._terminate.is_set():
+            try:
+                c = self._cmdQ.get(timeout=0.01)
+                print("[{}]-[RCV]: RECEIVED COMMAND: {}".format(Process.name, c))
+            except Empty:
+                pass
             e = randint(1, 5)
-            self._execQ.put(e)
-            print("[{}]-[SND]: SENT {}".format(Process.name, e))
+            self._rsltQ.put(e)
+            print("[{}]-[SND]: ANSWER SENT {}".format(Process.name, e))
             sleep(0.0001)
 
         print("[{}]-[MSG]: SHUTTING DOWN...".format(Process.name))
@@ -45,67 +77,59 @@ class Producer:
 
 class Receiver:
 
-    def __init__(self, execQ: Queue, terminate: Event):
-        self._execQ = execQ
-        self._procQ = Queue(maxsize=200)
+    def __init__(self, rsltQ: Queue, terminate: Event, cvPortFree: Condition, portFree: Event):
+        self._rsltQ = rsltQ
         self._terminate: Event = terminate
-        self._cvPortFree: Condition = Condition()
-        self._portFree: Event = Event()
+        self._cvPortFree = cvPortFree
+        self._portFree = portFree
         self._portFree.set()
-
-    def rec(self):
-        print("[{}]-[MSG]: STARTED...".format(Process.name))
-        while not self._terminate.is_set():
-            if not self._execQ.empty():
-                with self._cvPortFree:
-                    self._cvPortFree.wait_for(lambda: self._portFree.is_set())
-                    cmd = self._execQ.get()
-                    print("[{}]-[RCV]: RECEIVED {}...".format(Process.name, cmd))
-                    self._procQ.put(cmd)
-                    self._cvPortFree.notify_all()
-
-        print("[{}]-[MSG]: SHUTTING DOWN...".format(Process.name))
-        return
 
     def proc(self):
         print("[{}]-[MSG]: STARTED...".format(Process.name))
         while not self._terminate.is_set():
-            if self._procQ.empty():
-                self._portFree.set()
+            if self._rsltQ.empty():
+                sleep(0.001)
                 continue
 
-            m = self._procQ.get()
+            m = self._rsltQ.get()
             with self._cvPortFree:
-                self._portFree.clear()
                 if m == 5:
                     print("[{}]-[RCV]: OK {}...".format(Process.name, m))
                     self._portFree.set()
-                else:
+                if not m == 5:
                     print("[{}]-[RCV]: OCCUPIED {}...".format(Process.name, m))
                 self._cvPortFree.notify_all()
 
         print("[{}]-[MSG]: SHUTTING DOWN...".format(Process.name))
+        with self._cvPortFree:
+            self._portFree.set()
+            self._cvPortFree.notify_all()
         return
 
 
 if __name__ == '__main__':
     execQ = Queue(maxsize=200)
-    terminate = Event()
-    producer = Producer(execQ, terminate)
-    receiver = Receiver(execQ, terminate)
-    rec1 = Receiver(execQ, terminate)
-    p = Process(name="PRODUCER", target=producer.prod)
-    r = Process(name="RECEIVER", target=receiver.rec)
-    r1 = Process(name="RECEIVER", target=receiver.proc)
+    rsltQ = Queue(maxsize=500)
+    cvPortFree: Condition = Condition()
+    portFree: Event = Event()
+    portFree.set()
 
-    p.start()
+    terminate = Event()
+    commander = Commander(execQ, terminate, cvPortFree, portFree)
+    delegate = Delegate(execQ, rsltQ, terminate)
+    receiver = Receiver(rsltQ, terminate, cvPortFree, portFree)
+    c = Process(name="COMMANDER", target=commander.prod)
+    d = Process(name="DELEGATE", target=delegate.prod)
+    r = Process(name="RECEIVER", target=receiver.proc)
+
+    c.start()
+    d.start()
     r.start()
-    r1.start()
 
     sleep(30)
 
     terminate.set()
-    p.join()
+    c.join()
+    d.join()
     r.join()
-    r1.join()
     print("SHUT DOWN COMPLETE...")
