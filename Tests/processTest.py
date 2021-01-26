e44c9c462de2354331f4d8c9ca99c01f3ff7e1e7
Dietrich Christopeit <dietch@esper.co.uk>
2021-01-26 11:42:26 +0100

    Adapt multiprocessing to project

--- Tests/processTest.py
+++ Tests/processTest.py
@@ -1,19 +1,21 @@
-﻿from multiprocessing import Event
-from multiprocessing.context import Process
+﻿from multiprocessing import Process, Queue, Event
 from random import uniform
 from time import sleep
 
 
 class Proctest(Process):
-    def __init__(self, name: str, terminate: Event):
+    def __init__(self, name: str, terminate: Event, q: Queue):
         super().__init__()
         self._name = name
+        self._q = q
         self.terminate = terminate
         return
 
     def run(self):
         while not self.terminate.is_set():
-            print("Name: {}:{}".format(Process.name, self.name))
+            print("Name: {}:{}".format(Process.name, self._name))
+            if not self._q.qsize() == 0:
+                print("Queue: {}".format(self._q.get()))
             sleep(uniform(1, 3))
 
         print("Shutting down...")
@@ -22,9 +24,15 @@ class Proctest(Process):
 
 if __name__ == "__main__":
     terminate: Event = Event()
-    p: Proctest = Proctest("TESTPROCESS", terminate)
+    q: Queue = Queue()
+    p: Proctest = Proctest("TESTPROCESS", terminate, q)
 
     p.start()
+
+    for i in range(1, 10):
+        print("MAIN: putting {}".format(i))
+        q.put(i)
+        sleep(0.5)
 
     sleep(20)
 
