aea52e40b88ddb626746c25ad8636182720e8667
Dietrich Christopeit <dietch@esper.co.uk>
2021-02-04 21:59:49 +0100

    start trial with asyncio

--- Tests/ThreadPoolExecutor.py
+++ Tests/ThreadPoolExecutor.py
@@ -21,81 +21,25 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                   *
 #  SOFTWARE.                                                                                       *
 # **************************************************************************************************
+from concurrent.futures.thread import ThreadPoolExecutor
+from random import uniform
+from threading import Event
 
 
-import asyncio
-from asyncio import Event
-from asyncio import Queue
-
-from LegoBTLE.Device.messaging import Message
-
-
-class ABTLEDelegate:
-    """
-    Signal generator
-        * (PUBLISHER to AHub)
-        * employs a Q_SND_RETVAL_HUB (=== Q_RCV_RETVAL_BTLE in AHub) to publish CMD RETVALs to AHub
-            * sends messaging.Message as send container
-        * should PROBABLY be asyncio thread as I/O blocks
-        * (SUBSCRIBER to AHUB)
-            * receives bytearrays
-    """
-    
-    def __init__(self):
-        return
-    
-    def handleNotification(self, cHandle, data):  # Eigentliche Callbackfunktion
-        m: Message = Message(bytes.fromhex(data.hex()), True)
-        return
+def cpow(w):
+    for i in range(1, 50):
+        print("WORKER {} ".format(w))
+        Event().wait(uniform(.001, .002))
+    return
 
 
-class AHub:
-    """
-    COMMAND central:
-        * offers functions to register devices
-        * references a Q_RCV_CMD_DEVICE queue receiving commands issued TO the AHUB on a device (AMOTOR)
-            * (SUBSCRIBER TO DEVICES)
-            * receives messaging.Message as RECEIVE container
-        * employes a Q_SND_CMD_BTLE Queue sending commands gathered from all devices to BTLE
-            * (PUBLISHER TO BTLE)
-            * sends bytearrays
-        * employes a Q_RCV_RETVAL_BTLE Queue receiving notifications from BTLE as CMD result values
-            * (SUBSCRIBER TO BTLE)
-            * receives messaging.Message as RECEIVE container
-        * employes a dispatch function to distribute RETVALs to devices
-        * uses messaging.Message as SEND/RECEIVE container
-    """
-    
-    def __init__(self):
-        return
-
-
-class AMotor:
-    """
-    A Motor Device
-        * offers the available commands (functions) of the Device
-        * references a Q_SND_COMMAND_HUB (=== Q_RCV_CMD_DEVICE) to send CMDs (functions) to the AHub (PUBLISHER to AHUB)
-        * employs result variables (SUBSCRIBER to AHUB)
-        * uses messaging.Message as SEND/RECEIVE container
-    """
-    
-    def __init__(self):
-        return
-
-
-async def myfunction1():
-    while True:
-        await asyncio.sleep(1)
-        print("1 second passed")
-
-
-async def myfunction2():
-    while True:
-        await asyncio.sleep(2)
-        print("2 seconds passed")
-
-
-if __name__ == '__main__':
-    asyncio.ensure_future(myfunction1())
-    asyncio.ensure_future(myfunction2())
-    asyncio.get_event_loop().run_forever()
+with ThreadPoolExecutor(max_workers=10) as executor:
+    f0 = executor.submit(cpow, w=0)
+    f1 = executor.submit(cpow, w=1)
+    f2 = executor.submit(cpow, w=2)
+    f3 = executor.submit(cpow, w=3)
+    f4 = executor.submit(cpow, w=4)
+    f5 = executor.submit(cpow, w=5)
+    f6 = executor.submit(cpow, w=6)
+    f7 = executor.submit(cpow, w=7)
+    print(f0.result(), f1.result(), f2.result())
