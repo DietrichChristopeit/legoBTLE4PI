9a7657ada36c2c5d68a939162bbb5ff896e2b2da
DietrichChristopeit <dietch@esper.co.uk>
2021-02-15 00:00:56 +0100

    with TCP Server and Client: Client-Server Connection established

--- LegoBTLE/Device/messaging.py
+++ LegoBTLE/Device/messaging.py
@@ -133,7 +133,7 @@ class Message:
         :param payload:
             The byte sequence comprising the command.
         """
-        self._payload: bytearray = bytearray(payload + b' ')
+        self._payload: bytearray = bytearray(payload + b' ') if not payload.endswith(b' ') else bytearray(payload)
         
         self._length: int = self._payload[0]
         self._type = MESSAGE_TYPE.get(self._payload[2].to_bytes(1, 'little', signed=False), None)
